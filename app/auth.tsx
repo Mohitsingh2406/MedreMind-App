83a03185ec881daf1bade0ad8c24039c288d6cc7
Mohit Singh Tomar <mohitsingh2004624@gmail.com>
2025-01-30 13:12:17 +0530

    Update

---
+++ app/auth.tsx
@@ -0,0 +1,14 @@
+import { useState , useEffect } from "react";
+import { View,Text,StyleSheet,TouchableOpacity,Dimensions } from "react-native";
+import { useRouter } from "expo-router";
+import * as LocalAuthentication from 'expo-local-authentication';
+import { Ionicons } from "@expo/vector-icons";
+import {LinearGradient} from 'expo-linear-gradient';
+
+const {width} = Dimensions.get('window');
+
+export default function AuthScreen(){
+    return(
+        <LinearGradient colors={['#4CAF50', '#2E7D32']}></LinearGradient>
+    )
+}
